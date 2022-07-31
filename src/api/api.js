d4a330914788ceced60c0b208204dabdb904974b
Variandr <boltyn02@gmail.com>
2022-07-31 08:55:17 +0300

    feat: connection to remote BE, updated product styles

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: 'https://variandr-store-backend.herokuapp.com/',
     cache: new InMemoryCache()
 });
 
